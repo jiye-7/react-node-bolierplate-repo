b6306da23460904b8818aa909f104b3965f13d51
jiye <jy522hi@gmail.com>
2021-12-08 17:02:03 +0900

    Mongoose를 사용하여 MondoDB와 연결

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express'); // express module 가져오기
 const app = express(); // new express application make
 const port = 5000;
 
+const URL = `mongodb+srv://jiye:${password}@boilerplate.q2otx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(URL)
+  .then(() => console.log(`MongoDB Connected..!`))
+  .catch((err) => console.log(err));
+
 // root directory에 요청이 오면
 app.get('/', (request, response) => {
   response.send(`Hello World!`);
